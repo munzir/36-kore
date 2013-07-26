3fe10e379fe0b95cc2481cd537c710e3f37d5021
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2013-07-25 20:10:36 -0400

    fix timing bug in Kore sensors that was causing assert failures

--- sensors.cpp
+++ sensors.cpp
@@ -132,14 +132,23 @@ void getImu (ach_channel_t* imuChan, double& _imu, double& _imuSpeed, double dt,
 	// ======================================================================
 	// Get the readings
 
-	// Get the value
-	int r;
-	struct timespec currTime;
-	clock_gettime(CLOCK_MONOTONIC, &currTime);
-	struct timespec abstime = aa_tm_add(aa_tm_sec2timespec(1.0/30.0), currTime);
-	Somatic__Vector *imu_msg = SOMATIC_WAIT_LAST_UNPACK(r, somatic__vector, 
-			&protobuf_c_system_allocator, IMU_CHANNEL_SIZE, imuChan, &abstime );
-	assert((imu_msg != NULL) && "Imu message is faulty!");
+    // Get a message
+    int r;
+    struct timespec timeout = aa_tm_future(aa_tm_sec2timespec(1.0/30.0));
+    Somatic__Vector *imu_msg = SOMATIC_WAIT_LAST_UNPACK(r, somatic__vector, 
+                                                        &protobuf_c_system_allocator,
+                                                        IMU_CHANNEL_SIZE,
+                                                        imuChan,
+                                                        &timeout);
+    assert((imu_msg != NULL) && "Didn't get IMU message!");
+	// // Get the value
+	// int r;
+	// struct timespec currTime;
+	// clock_gettime(CLOCK_MONOTONIC, &currTime);
+	// struct timespec abstime = aa_tm_add(aa_tm_sec2timespec(1.0/30.0), currTime);
+	// Somatic__Vector *imu_msg = SOMATIC_WAIT_LAST_UNPACK(r, somatic__vector, 
+	// 		&protobuf_c_system_allocator, IMU_CHANNEL_SIZE, imuChan, &abstime );
+	// assert((imu_msg != NULL) && "Imu message is faulty!");
 
 	// Get the imu position and velocity value from the readings (note imu mounted at 45 deg).
 	static const double mountAngle = -.7853981634;
