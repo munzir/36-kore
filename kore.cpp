1f644e7898fd8d1d738018f7044460066b4e758b
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2013-07-31 00:33:42 +0100

    better hardware state output in curses

--- kore.cpp
+++ kore.cpp
@@ -324,8 +324,25 @@ void Hardware::printState () {
 		mvprintw(row+1, col+16, "waist: %.3lf", s(imuWaist_ids[1]));
 		mvprintw(row+1, col+31, "torso: %.3lf", s(9));
 		
+		// display amc currents
+		char bar_buffer[1024];
+		int bar_len = 80;
+		double amc_max = 40;
+		for(int mot = 0; mot < amc->n; mot++) {
+			int i = 0;
+			for(; i < bar_len * amc->cur[mot] / amc_max; i++) bar_buffer[bar_len + i] = '|';
+			for(; i < bar_len; i++) bar_buffer[bar_len + i] = ' ';
+			i = 0;
+			for(; i < bar_len * -amc->cur[mot] / amc_max; i++) bar_buffer[bar_len - i] = '|';
+			for(; i < bar_len; i++) bar_buffer[bar_len - i] = ' ';
+			bar_buffer[2*bar_len-1] = '\0';
+			bar_buffer[0] = '[';
+			bar_buffer[2*bar_len-2] = ']';
+			mvprintw(row+3+mot, col, bar_buffer);
+		}
+
 		// print out the left arm, with pretty colors for joint limits
-		mvprintw(row+3, col+2, "left arm:");
+		mvprintw(row+6, col+2, "left arm:");
 		kinematics::Dof* dof;
 		double dist;
 		for(int i = 0; i < 7; i++) {
@@ -333,18 +350,18 @@ void Hardware::printState () {
 			dist = std::min(std::abs(s[left_arm_ids[i]] - dof->getMin()),
 			                std::abs(s[left_arm_ids[i]] - dof->getMax()));
 			if (dist < .25) attron(COLOR_PAIR(COLOR_RED_BACKGROUND));
-			mvprintw(row+3, col+14+(i*12), "%.8lf", s(left_arm_ids[i]));
+			mvprintw(row+6, col+14+(i*12), "%.8lf", s(left_arm_ids[i]));
 			if (dist < .25) attroff(COLOR_PAIR(COLOR_RED_BACKGROUND));
 		}
 
 		// the same for the right arm. TODO: unify these
-		mvprintw(row+4, col+2, "right arm:");
+		mvprintw(row+7, col+2, "right arm:");
 		for(int i = 0; i < 7; i++) {
 			dof = robot->getDof(right_arm_ids[i]);
 			dist = std::min(std::abs(s[right_arm_ids[i]] - dof->getMin()),
 			                std::abs(s[right_arm_ids[i]] - dof->getMax()));
 			if (dist < .25) attron(COLOR_PAIR(COLOR_RED_BACKGROUND));
-			mvprintw(row+4, col+14+(i*12), "%.8lf", s(right_arm_ids[i]));
+			mvprintw(row+7, col+14+(i*12), "%.8lf", s(right_arm_ids[i]));
 			if (dist < .25) attroff(COLOR_PAIR(COLOR_RED_BACKGROUND));
 		}
 	}
